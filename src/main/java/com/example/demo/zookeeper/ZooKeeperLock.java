b53dcd07e336a1e779d8859fdb0d7369d8fc32e2
songjie <364063862@qq.com>
2020-04-16 23:10:13 +0800

    zookeeper 分布式锁

--- src/main/java/com/example/demo/zookeeper/ZooKeeperLock.java
+++ src/main/java/com/example/demo/zookeeper/ZooKeeperLock.java
@@ -1,8 +1,11 @@
 package com.example.demo.zookeeper;
 
 
+import org.I0Itec.zkclient.IZkDataListener;
 import org.I0Itec.zkclient.ZkClient;
 import org.apache.zookeeper.CreateMode;
+
+import java.util.List;
 
 /**
  * 创建临时的循序节点。
@@ -52,6 +55,22 @@ public class ZooKeeperLock {
         ZkClient zkClient = new ZkClient("111.230.99.82", 5000, 20000);
        // zkClient.create("/songjie-lock","1314", CreateMode.PERSISTENT);
         String path = zkClient.createEphemeralSequential("/songjie-lock/zkdemo", "w");
+        //获取子节点
+        List<String> children = zkClient.getChildren("");
+
+        String childrenPath = children.get(0);
+        zkClient.subscribeDataChanges(childrenPath, new IZkDataListener(){
+            @Override
+            public void handleDataChange(String s, Object o) throws Exception {
+                //数据修改后处理
+            }
+
+            @Override
+            public void handleDataDeleted(String s) throws Exception {
+                //数据修改删除
+                
+            }
+        });
         System.out.println(path);
     }
 }
